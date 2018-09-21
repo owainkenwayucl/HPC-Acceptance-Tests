7e527365fa2a870f734331e339b1ccf98e467b2b
Owain Kenway <o.kenway@ucl.ac.uk>
2018-09-21 11:26:02 +0100

    Thomas/Michael acceptance tests.

---
+++ Applications/LAMMPS/at.thomas.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+# Job script for running LAMMPS as an acceptance test on UCL software stack.
+# Owain Kenway
+
+#$ -S /bin/bash
+
+#$ -l h_rt=12:0:0
+
+#$ -l mem=1G
+
+#$ -N lammps-12hr
+
+#$ -cwd
+
+# Modify following according to what is suitable for this architecture.
+#$ -pe mpi 72
+
+module load rcps-core/1.0.0
+module load compilers/intel/2018
+module load mpi/intel/2018
+module load lammps/16Mar18/basic/intel-2018
+# module load lammps/16Mar18/userintel/intel-2018 # Intel optimised alternative
+
+gerun  lmp_default -in in.rhodo.at.thomas -log log.at.$JOB_ID
+# gerun  lmp_default -sf intel -in in.rhodo.at.thomas -log log.at.$JOB_ID # Intel optimised alternative
