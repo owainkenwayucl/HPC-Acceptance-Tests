eec6f04ccfe52b5d591fb68d35b71d5d34b5c276
Owain Kenway <o.kenway@ucl.ac.uk>
2020-03-17 14:25:40 +0000

    Contain stuff in subdirectories
    In my testing on Myriad this is already a mess.

--- Applications/LAMMPS/HECBioSim/fetch.sh
+++ Applications/LAMMPS/HECBioSim/fetch.sh
@@ -7,10 +7,14 @@ url="http://www.hecbiosim.ac.uk/lammps-benchmarks/send/2-software/10-lammps-benc
 md5="65c3b50b77688611e92fcff330e27e48"
 sha1="0d520e86fda2886a5faca4e1d1171a666a35fe4c"
 
+mkdir downloads
+cd downloads
 wget "$url"
 
 md5sum -c <<< "$md5 10-lammps-bench" 
 sha1sum -c <<< "$sha1 10-lammps-bench"
 
-tar xvf 10-lammps-bench
+cd ..
+
+tar xvf downloads/10-lammps-bench
 
